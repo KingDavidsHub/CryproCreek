9a3500f049da59b87d7ea077b0e2b7753cabbeae
KingDavid <ezehdavidifeanyi1234@gmail.com>
2024-03-02 03:56:24 +0100

    u

---
+++ routes/user.auth.router.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+const { signin, signup, verifyToken, isVerified, forgotPassWord, resendCode, resetPassword, changePassWord} = require('../controllers/user.auth.controller')
+const { protect, userRoleAuth } = require('../../middleware/auth.middleware')
+router.route('/signup').post(signup)
+router.route('/signin').post(signin)
+router.route('/verifyToken').post(verifyToken)
+router.route('/isVerified/:userId').get(isVerified)
+router.route('/forgotPassWord').post(forgotPassWord)
+router.route('/resendCode').post(resendCode)
+router.route('/changePassWord').post(protect, changePassWord)
+router.route('/resetPassWord').put(resetPassword)
+
+
+module.exports = router;
